826526d340eb0493766d2f6fb22bb544b206967e
ashcse <ashcse.khare@gmail.com>
2017-04-06 17:03:48 +0530

    Created project to understand attriute routing

---
+++ BookApi/DTOs/BookDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookApi.DTOs
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+    }
+}
